4ba635c98d9c6b87d694e90ecf3c596245c3a56b
ds-joy <debasishsarker36@gmail.com>
2019-09-16 00:12:23 +0600

    Day06 accepted

--- Day06/Solution.java
+++ Day06/Solution.java
@@ -1,21 +1,23 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
     private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         
-
         int t;
         int l;
         t = scan.nextInt();
-        if(scan.hasnextLine()) {
-            scan.nextLine;
-        }
+        scan.nextLine();
         String str;
         
-        while(t>=0) {
+        while(t>0) {
             
             str = scan.nextLine();
             l = str.length();
